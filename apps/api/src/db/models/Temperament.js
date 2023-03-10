7d1b99698b7a27eeec4183589f63e3e4760fd456
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-03-10 20:07:34 -0300

    setup database (#2)

---
+++ apps/api/src/db/models/Temperament.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "temperaments",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamp: false,
+    }
+  );
+};
--- apps/api/src/start.js
+++ apps/api/src/start.js
@@ -1,13 +1,16 @@
-if (process.env.NODE_ENV === "development") {
-  require("dotenv").config();
-}
 const logger = require("loglevel");
 const server = require("./server");
-
-const APP_PORT = process.env.APP_PORT || 3001;
+const { APP_PORT, DB_FORCE } = require("./config");
+const { conn } = require("./db");
 
 logger.setLevel(process.env.NODE_ENV !== "production" ? "TRACE" : "SILENT");
 
 server.listen(APP_PORT, async () => {
   logger.info(`Server listening at ${server.address().port}`);
+  try {
+    await conn.sync({ force: DB_FORCE });
+    logger.info("Database is up");
+  } catch (err) {
+    logger.error(err.message);
+  }
 });
